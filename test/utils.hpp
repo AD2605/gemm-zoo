2dee4551973ee6eeaea54a68cc32db9edc0d6985
Atharva Dubey <atharva.dubey@codeplay.com>
2025-06-12 19:34:10 +0000

    Revert "switch to oneDNN"
    This reverts commit d34ec146e06727c6e8e7dcca010fc6cc4387a3eb

--- test/utils.hpp
+++ test/utils.hpp
@@ -1,14 +1,8 @@
 #ifndef TEST_UTILS_HPP
 #define TEST_UTILS_HPP
 
-#include "oneapi/dnnl/dnnl.hpp"
-#include "oneapi/dnnl/dnnl_common.hpp"
-#include "oneapi/dnnl/dnnl_sycl.hpp"
-#include "oneapi/dnnl/dnnl_types.h"
 #include <cstddef>
 #include <cstdint>
-#include <dnnl.hpp>
-#include <dnnl_sycl.hpp>
 
 #include <oneapi/math.hpp>
 #include <oneapi/math/rng/device.hpp>
@@ -63,74 +57,15 @@ bool compare_results(const T* output, const T* reference,
   return is_passed;
 }
 
-template <typename T>
-dnnl::memory::data_type dnnl_memory_data_type() {
-  if constexpr (std::is_same_v<T, float>) {
-    return dnnl::memory::data_type::f32;
-  }
-  if constexpr (std::is_same_v<T, sycl::half>) {
-    return dnnl::memory::data_type::f16;
-  }
-  if constexpr (std::is_same_v<T, sycl::ext::oneapi::bfloat16>) {
-    return dnnl::memory::data_type::bf16;
-  }
-  if constexpr (std::is_same_v<T, int32_t>) {
-    return dnnl::memory::data_type::s32;
-  }
-  if constexpr (std::is_same_v<T, int8_t>) {
-    return dnnl::memory::data_type::s8;
-  }
-  if constexpr (std::is_same_v<T, uint8_t>) {
-    return dnnl::memory::data_type::u8;
-  }
-}
-
 template <typename TIn, typename TOut>
 void compute_reference(const TIn* a, const TIn* b, TOut* c, int m, int n, int k,
                        const TOut alpha, const TOut beta, sycl::queue& queue) {
-  // using oneDNN here because oneMath unfortunately does not
-  // provide different data type combinations.
-
-  auto device = queue.get_device();
-  auto context = queue.get_context();
-  dnnl::engine engine = dnnl::sycl_interop::make_engine(device, context);
-  dnnl::stream stream = dnnl::sycl_interop::make_stream(engine, queue);
-
-  dnnl::memory::desc a_desc = dnnl::memory::desc(
-      {m, k}, dnnl_memory_data_type<TIn>(), dnnl::memory::format_tag::ab);
-  dnnl::memory::desc b_desc = dnnl::memory::desc(
-      {k, n}, dnnl_memory_data_type<TIn>(), dnnl::memory::format_tag::ab);
-  dnnl::memory::desc c_desc = dnnl::memory::desc(
-      {m, n}, dnnl_memory_data_type<TOut>(), dnnl::memory::format_tag::ab);
-
-  dnnl::primitive_attr matmul_attributes;
-  matmul_attributes.set_scratchpad_mode(dnnl::scratchpad_mode::library);
-  matmul_attributes.set_fpmath_mode(dnnl::fpmath_mode::any);
-  matmul_attributes.set_scales_mask(DNNL_ARG_WEIGHTS, 0);
-  dnnl::post_ops post_ops;
-  post_ops.append_sum(beta, 0, dnnl_memory_data_type<TOut>());
-  matmul_attributes.set_post_ops(post_ops);
-
-  dnnl::matmul::primitive_desc matmul_pd(engine, a_desc, b_desc, c_desc,
-                                         matmul_attributes);
-  auto matmul_primitive = dnnl::matmul(matmul_pd);
-
-  dnnl::memory a_memory(a_desc, engine, (void*)a);
-  dnnl::memory b_memory(b_desc, engine, (void*)b);
-  dnnl::memory c_memory(c_desc, engine, (void*)c);
-
-  TOut* alpha_memory = sycl::malloc_device<TOut>(1, queue);
-  queue.copy(&alpha, alpha_memory, 1).wait_and_throw();
-  dnnl::memory::desc alpha_desc({1}, dnnl_memory_data_type<TOut>(),
-                                dnnl::memory::format_tag::a);
-  dnnl::memory alpha_memory_object(alpha_desc, engine, (void*)alpha_memory);
-
-  matmul_primitive.execute(
-      stream, {{DNNL_ARG_SRC, a_memory},
-               {DNNL_ARG_WEIGHTS, b_memory},
-               {DNNL_ARG_DST, c_memory},
-               {DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS, alpha_memory_object}});
-  stream.wait();
+  // TODO: pass a matrix config POD struct here, containing
+  // layout, and leading dimensions etc...
+  // For now, just assume row major and matrix width = lda
+  auto no_trans = oneapi::math::transpose::nontrans;
+  oneapi::math::blas::row_major::gemm(queue, no_trans, no_trans, m, n, k, alpha,
+                                      a, k, b, n, beta, c, n);
   queue.wait_and_throw();
 }
 
