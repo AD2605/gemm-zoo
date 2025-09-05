d7cd17796e9ad5a9682e6c326a895b24034a11a6
atharva.dubey <atharva.dubey@codeplay.com>
2025-09-05 11:11:57 +0100

    add cublaslt bench file

---
+++ nvidia/bench/bench_cublaslt.cu
@@ -0,0 +1,17 @@
+#include "kernel_functors/cublaslt_gemm.cuh"
+#include "utils/benchmark.cuh"
+
+#include <cuda_runtime.h>
+
+#include <tuple>
+
+int main() {
+  float alpha = 1.0f;
+  float beta = 1.0f;
+
+  using kernel_functor_struct =
+      nvidia::kernel_functors::cublasLt_gemm<float, float>;
+  benchmark::benchmark<kernel_functor_struct, float, float>(
+      {{1024, 1024, 1024}, {2048, 2048, 2048}, {4096, 4096, 4096}}, alpha,
+      beta);
+}
